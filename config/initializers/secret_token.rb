142ac2f2a166d168f519afcc2fef919a076f71da
Glenn Goodrich <glenn.goodrich@gmail.com>
2011-10-26 05:51:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qfeedreader::Application.config.secret_token = '57c1452f56970cfc94f8be5974034ca142450774a539ffcdaba97f158696cac3730e3b969b9191a3a7d073015e8ab41aa3b39e5d512f35e0991d470335ac36c8'
